4caa36f20d71717d33bcc1f2eb63ce6325a0779a
AlexanderAldama-Apodaca <130171945+AlexanderAldama-Apodaca@users.noreply.github.com>
2026-06-01 19:15:04 -0400

    Update test_convoy_disruption.py
    Updated test_convoy_disruption.py to version that accurately reflects the Colonial Diplomacy board.

--- test_convoy_disruption.py
+++ test_convoy_disruption.py
@@ -10,18 +10,22 @@ def test_convoy_disruption():
     env.units[1].append({"type": "Army", "location": "Ceylon"})
 
     # Convoy fleet
-    env.units[1].append({"type": "Fleet", "location": "Bay_of_Bengal"})
+    env.units[1].append({"type": "Fleet", "location": "Gulf_of_Manaar"})
 
-    # Enemy fleet attacking convoy
+    # Enemy fleets attacking convoy
     env.units[2].append({"type": "Fleet", "location": "Andaman_Sea"})
+    env.units[2].append({"type": "Fleet", "location": "Bay_of_Bengal"})
 
     orders = {
         1: [
-            Order(1, "Ceylon", "MOVE", target="Rangoon", via_convoy=True),
-            Order(1, "Bay_of_Bengal", "CONVOY", convoy_origin="Ceylon", convoy_destination="Rangoon")
+            Order(1, "Ceylon", "MOVE", target="Madras", via_convoy=True),
+            Order(1, "Gulf_of_Manaar", "CONVOY", convoy_origin="Ceylon", convoy_destination="Madras")
         ],
 
-        2: [Order(2, "Andaman_Sea", "MOVE", target="Bay_of_Bengal")]
+        2: [
+            Order(2, "Andaman_Sea", "MOVE", target="Gulf_of_Manaar"),
+            Order(2, "Bay_of_Bengal", "SUPPORT", support_unit="Andaman_Sea", support_target="Gulf_of_Manaar")
+        ]
     }
 
     env.submit_orders(orders)
