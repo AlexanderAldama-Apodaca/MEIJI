cdd66cb977c88ed71dd5be27047d04470b4d8c8f
AlexanderAldama-Apodaca <130171945+AlexanderAldama-Apodaca@users.noreply.github.com>
2026-06-05 20:00:41 -0400

    Update test_tsr_movement.py
    Added an illegal TSR move to test_tsr_movement.py.

--- test_tsr_movement.py
+++ test_tsr_movement.py
@@ -8,8 +8,13 @@ def test_tsr_movement():
 
     env.units[6].append({"type": "Army", "location": "Omsk"})
 
+    env.units[6].append({"type": "Army", "location": "Vladivostok"})
+
     orders = {
-        6: [Order(6, "Omsk", "MOVE", target="Irkutsk", via_tsr=True)]
+        6: [
+            Order(6, "Omsk", "MOVE", target="Irkutsk", via_tsr=True),           # Legal TSR move
+            Order(6, "Vladivostok", "MOVE", target="Port_Arthur", via_tsr=True) # Illegal TSR move
+        ]
     }
 
     env.submit_orders(orders)
@@ -21,6 +26,13 @@ def test_tsr_movement():
     assert pid == 6
 
     print("test_tsr_movement passed")
+
+    pid, unit = env.get_unit_at("Vladivostok")
+
+    assert pid == 6
+    assert unit is not None
+
+    print("test_invalid_tsr_passed")
 
 if __name__ == "__main__":
     test_tsr_movement()
